24dab96420e9fe52f31d8fc678264a573b690d76
LindholmGithub <christianlindholm@jubii.dk>
2021-09-02 14:37:14 +0200

    Added startup for WebAPI

---
+++ PetShop.WebAPI/Controllers/PetController.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PetShop.Core.IServices;
+using PetShop.Core.Models;
+
+namespace PetShop.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+        private readonly IPetService _petService;
+
+        public PetController(IPetService petService)
+        {
+            _petService = petService;
+        }
+
+        [HttpGet]
+        public List<Pet> getAllPets()
+        {
+            return _petService.GetAllPets();
+        }
+    }
+}
